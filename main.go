f93c90a79ebeee368419861148e379013d3fab54
ashish primelab <ashish.singla@primelab.io>
2022-05-26 17:49:17 +0530

    read env from .env file

--- main.go
+++ main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"log"
 	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/tensor-programming/golang-blockchain/cli"
 )
 
 func main() {
 	defer os.Exit(0)
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
 	// api := api.Api{}
 	// api.Run()
